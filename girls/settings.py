cd6bab7350ce4bea695226fd9dd04c1dba2a7e9b
Princi Gupta <princygupta037@gmail.com>
2019-12-17 15:28:17 +0530

    detail-post

--- girls/settings.py
+++ girls/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'girl',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
