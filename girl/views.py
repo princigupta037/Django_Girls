72dd78f07cd36548ce22ca4e61991a946f16cd76
Princi Gupta <princygupta037@gmail.com>
2019-12-18 10:40:12 +0530

    girls

--- girl/views.py
+++ girl/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
@@ -12,7 +13,16 @@ def post_details(request, pk):
     return render(request, 'post_details.html' , {'post':post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'post_edit.html', {'form': form})
 
 def post_edit(request, pk):
@@ -27,4 +37,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'post_edit.html', {'form': form})
